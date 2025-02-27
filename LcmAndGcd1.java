1250bc20b74755ede7ae0476a738fe4413a63991
Akankshapendem <168049422+Akankshapendem@users.noreply.github.com>
2025-02-27 06:51:36 +0530

    Add files via upload

---
+++ LcmAndGcd1.java
@@ -0,0 +1,14 @@
+class LcmAndGcd1 {
+    public static int[] lcmAndGcd(int a, int b) {
+        // code here
+        int gcdValue=gcd(a,b);
+        int lcm=Math.abs(a*b)/gcdValue;
+        return new int[]{lcm,gcdValue};
+    }
+    public static int gcd(int a ,int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+}
